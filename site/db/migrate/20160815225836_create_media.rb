3b47a714960a0fb5354c3b2c3dbdf0f153101883
Rubisel Prieto Dupeyron <rubisel.prieto@datys.cu>
2016-10-14 09:20:07 -0400

    Fix index

--- site/db/migrate/20160815225836_create_media.rb
+++ site/db/migrate/20160815225836_create_media.rb
@@ -3,6 +3,7 @@ class CreateMedia < ActiveRecord::Migration
     create_table :media do |t|
       t.timestamps null: false
       t.boolean :inline ,:default => false
+      t.attachment :file
       t.references :tags, index: true, foreign_key: true
       t.references :user, index: true, foreign_key: true
     end
