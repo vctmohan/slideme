43f14ec322974b18ab9f7cce5c24072b41a96f77
Rubisel Prieto Dupeyron <rubisel.prieto@datys.cu>
2016-10-14 14:14:42 -0400

    Agregando base de datos

---
+++ site/.bashrc
@@ -0,0 +1,1 @@
+SSL_CERT_FILE=~/.cert/nexus.pem
---
+++ site/config/deploy.rb
@@ -0,0 +1,20 @@
+# config valid only for current version of Capistrano
+lock '3.6.1'
+
+set :application, 'slideme'
+set :repo_url, 'git@gitd7.datys.cu:devops/slideme.git'
+
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+set :use_sudo, false
+set :bundle_binstubs, nil
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+after 'deploy:publishing', 'deploy:restart'
+
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:reload'
+  end
+end
