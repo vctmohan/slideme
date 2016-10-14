eee45e7c314aa586525d5deb6fe19356505cd995
Rubisel Prieto Dupeyron <rubisel.prieto@datys.cu>
2016-10-14 09:17:32 -0400

    Fix index

--- site/db/migrate/20160815225736_create_media.rb
+++ site/db/migrate/20160815225736_create_media.rb
@@ -3,7 +3,8 @@ class CreateMedia < ActiveRecord::Migration
     create_table :media do |t|
       t.timestamps null: false
       t.boolean :inline ,:default => false
-      t.references :tags, :user, index: true, foreign_key: true
+      t.references :tags, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
     end
     #add_index :media, [:tags, :created_at]
   end
